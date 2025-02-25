05795878f8ad251dbc7d4e5feaabd7604051d8fd
Vinal Dsouza <dsouza.vi@northeastern.edu>
2025-02-24 21:31:54 -0800

    Remove index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-import { loadData } from "./src/AirBnBDataHandler.js";
-
-const filePath = "data/listings.csv"; // Make sure this file exists before running
-
-loadData(filePath).then((data) => {
-    if (data) {
-        console.log("File Content (First 500 chars):\n", data.substring(0, 500));
-    }
-});
